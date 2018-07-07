d9d1e8be5c6c36913ff9d9a5de11a41a3a577d4e
fuzzythecat <fuzzy0427@gmail.com>
2018-07-08 05:47:48 +0900

    Add first working gui
    This is just a skeleton code, with working `load_image` function

---
+++ main.py
@@ -0,0 +1,17 @@
+import sys
+from PyQt5.QtWidgets import QApplication
+
+from gui.pokedex import QPokedex
+
+
+def main():
+    app = QApplication(sys.argv)
+
+    window = QPokedex(745, 540)
+
+    window.show()
+    return app.exec_()
+
+
+if __name__ == "__main__":
+    main()
